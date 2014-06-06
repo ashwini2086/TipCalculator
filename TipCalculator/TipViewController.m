54eaf8b14c13be40a69e2f741f74e278b04f4510
ashwini satyanarayana <ashwini.satyanarayan@gmail.com>
2014-06-05 21:38:53 -0700

    Formating changes

--- TipCalculator/TipViewController.m
+++ TipCalculator/TipViewController.m
@@ -32,6 +32,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"tips.jpg"]];
 }
 
 - (void)didReceiveMemoryWarning
